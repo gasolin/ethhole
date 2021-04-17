7fde14ac990067dad2c0117f9adf4e7d1463607a
Fred Lin <gasolin@gmail.com>
2021-04-18 01:49:26 +0800

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        // check: "url('/icons/check.svg')",
+        // landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+}
