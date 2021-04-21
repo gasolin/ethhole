284487d6994aee8f9654c67172bc49d8d4a49a2d
Fred Lin <gasolin@gmail.com>
2021-04-21 22:18:26 +0800

    add yesterdayData reference

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,10 +1,25 @@
-import { timestamp } from '../data/data'
-
 export function getTimeStamp(tag = false) {
   const now = new Date()
   return tag ? now.toISOString().split('T')[0] : now.toISOString()
 }
 
-export function getTodayTag() {
-  return (new Date(timestamp)).toISOString().split('T')[0]
+export function getTimeTag(day, timestamp) {
+  const date = new Date(timestamp)
+  let time
+  switch(day) {
+    case 'yesterday':
+      time = date.setDate(date.getDate() - 1)
+      break
+    case 'week':
+      time = date.setDate(date.getDate() - 7)
+      break
+    case 'month':
+      time = date.setDate(date.getDate() - 30)
+      break
+    case 'today':
+    default:
+      time = date
+      break
+  }
+  return time.toISOString().split('T')[0]
 }
