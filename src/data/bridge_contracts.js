acf3b1b64212b56b4aaa557efa6fe45c4db46ed1
Fred Lin <gasolin@gmail.com>
2021-05-01 22:31:28 +0800

    fix typo

--- src/data/bridge_contracts.js
+++ src/data/bridge_contracts.js
@@ -327,15 +327,15 @@ export const ETH_BRIDGE_CONTRACTS = {
       { // also use for BSC tokens
         "address": "0xC564EE9f21Ed8A2d8E7e76c085740d5e4c5FaFbE",
         "chainId": MAINNET,
-        "name": "anyswap Fantom bridge",
+        "name": "Anyswap Fantom bridge",
         "project": PROJECT_ANYSWAP.name
       },
-      // {
-      //   "address": "0xBe008e52c5682A49dd0260735a26Aa221f303456",
-      //   "chainId": MAINNET,
-      //   "name": "connext Fantom bridge",
-      //   "project": PROJECT_CONNEXT.name
-      // },
+      {
+        "address": "0xBe008e52c5682A49dd0260735a26Aa221f303456",
+        "chainId": MAINNET,
+        "name": "Anyswap Fantom bridge",
+        "project": PROJECT_ANYSWAP.name
+      },
     ],
     ...PROJECT_FANTOM
   },
