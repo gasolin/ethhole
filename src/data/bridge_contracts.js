f78443d95f088d9b5621748c9623e66123f077b3
Fred Lin <gasolin@gmail.com>
2021-06-18 11:19:32 +0800

    📖 add data

--- src/data/bridge_contracts.js
+++ src/data/bridge_contracts.js
@@ -333,9 +333,15 @@ export const ETH_BRIDGE_CONTRACTS = {
   [PROJECT_ARBITRUM.name]: {
     "bridges": [
       {
+        "address": "0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515",
+        "chainId": MAINNET,
+        "name": "Ethereum Bridge",
+        "website": "https://offchainlabs.com/"
+      },
+      {
         "address": "0x86E525A3565235b2bb934df43E01c8B092c03756",
         "chainId": MAINNET,
-        "name": "Arbitrum",
+        "name": "ERC20 Bridge",
         "website": "https://offchainlabs.com/"
       }
     ],
