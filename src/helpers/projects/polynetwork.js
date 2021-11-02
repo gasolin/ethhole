e10b852ed149d14c1c0a4e09f59e10b8401bc352
Fred Lin <gasolin@gmail.com>
2021-11-02 18:23:37 +0800

    blacklist pONT on poly

--- src/helpers/projects/polynetwork.js
+++ src/helpers/projects/polynetwork.js
@@ -15,6 +15,9 @@ export const project = {
       "address": "0x250e76987d838a75310c34bf422ea9f1AC4Cc906",
       "name": "Lock Proxy",
       "chainId": MAINNET,
+      "excludeAddr": [
+        "0xcb46c550539ac3db72dc7af7c89b11c306c727c2", //pONT
+      ],
     },
     // {
     //   "address": "0x2f7ac9436ba4B548f9582af91CA1Ef02cd2F1f03",
