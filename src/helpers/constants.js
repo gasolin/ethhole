29b4a66fcfb95b9b4a7028655be7d35d79171795
Fred Lin <gasolin@gmail.com>
2021-11-16 22:14:35 +0800

    higher bar

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
 export const FILLTER = 1000 // USD
-export const FLOW_THRESHOLD = 300000 // ETH
-export const L2_FLOW_THRESHOLD = 6000
+export const FLOW_THRESHOLD = 500000 // ETH
+export const L2_FLOW_THRESHOLD = 20000
 export const MAX_AMOUNT = 100000000
--- src/helpers/projects/arbitrum.js
+++ src/helpers/projects/arbitrum.js
@@ -9,17 +9,18 @@ export const project = {
   twitter: "https://twitter.com/OffchainLabs",
   color: "#9e5ddb",
   type: TYPE_LAYER2,
+  // https://developer.offchainlabs.com/docs/useful_addresses
   bridges: [
     {
       "address": "0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515",
       "chainId": MAINNET,
-      "name": "Ethereum Bridge",
+      "name": "Wrapped Eth Bridge",
       "website": "https://bridge.arbitrum.io/"
     },
     {
       "address": "0xa3A7B6F88361F48403514059F1F16C8E78d60EeC",
       "chainId": MAINNET,
-      "name": "ERC20 Bridge",
+      "name": "ERC20 Gateway",
       "website": "https://bridge.arbitrum.io/"
     },
     {
