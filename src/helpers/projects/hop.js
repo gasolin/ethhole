526e5cd30a73d57bdb06e2792cbaa92cbe24333c
Fred Lin <gasolin@gmail.com>
2022-01-23 22:18:09 +0800

    update hop

--- src/helpers/projects/hop.js
+++ src/helpers/projects/hop.js
@@ -1,4 +1,4 @@
-import { TYPE_CROSSCHAIN_DEX } from '../projectTypes.js'
+import { TYPE_LIQUIDITY } from '../projectTypes.js'
 import { MAINNET } from '../chainInfo.js'
 
 export const project = {
@@ -7,13 +7,33 @@ export const project = {
   twitter: 'https://twitter.com/HopProtocol',
   github: 'https://github.com/hop-protocol',
   color: '#B32EFF',
-  type: TYPE_CROSSCHAIN_DEX,
+  type: TYPE_LIQUIDITY,
   bridges: [
     {
       "address": "0x3666f603Cc164936C1b87e207F36BEBa4AC5f18a",
       "chainId": MAINNET,
       "name": "Hop Exchange Bridge"
-    }
+    },
+    {
+      "address": "0x3E4a3a4796d16c0Cd582C382691998f7c06420B6",
+      "chainId": MAINNET,
+      "name": "Hop exchange: USDt Bridge"
+    },
+    {
+      "address": "0x3666f603Cc164936C1b87e207F36BEBa4AC5f18a",
+      "chainId": MAINNET,
+      "name": "Hop exchange: USDC Bridge"
+    },
+    {
+      "address": "0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1",
+      "chainId": MAINNET,
+      "name": "Hop exchange: DAI Bridge"
+    },
+    {
+      "address": "0x22B1Cbb8D98a01a3B71D034BB899775A76Eb1cc2",
+      "chainId": MAINNET,
+      "name": "Hop exchange: MATIC Bridge"
+    },
   ]
 }
 
